3f9705641fb70f2d368678002fa9ad7ccbdb9146
Seyed Yahya Shirazi <shirazi@ieee.org>
2023-08-05 19:53:30 +0330

    first setup.py

--- setup.py
+++ setup.py
@@ -15,23 +15,39 @@ SETUP_REQUIRES = ['setuptools >= 24.2.0']
 # This enables setuptools to install wheel on-the-fly
 SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
 
-opts = dict(name=NAME,
-            maintainer=MAINTAINER,
-            maintainer_email=MAINTAINER_EMAIL,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
-            license=LICENSE,
-            classifiers=CLASSIFIERS,
-            author=AUTHOR,
-            author_email=AUTHOR_EMAIL,
-            platforms=PLATFORMS,
-            version=VERSION,
+REQUIRES = [
+        'python>=3.9'
+        'seaborn>=0.12',
+        'matplotlib',
+        'numpy>=1.13',
+        'pandas>=1.2'
+        ]
+
+opts = dict(name='lineNdots',
+            version='0.0.1',
+            description='A Seaborn-Pyplot package to plot group-level reuslts, as an alternative to box plots.',
+            long_description=open('README.md').read(),
+            url='https://github.com/neuromechanist/lineNdots',
+            maintainer='Seyed (Yahya) Shirazi',
+            maintainer_email='shirazi@ieee.org',
+            author='Seyed (Yahya) Shirazi',
+            author_email='shirazi@ieee.org',
+            license='MIT',
+            classifiers=[
+                'Intended Audience :: Science/Research',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved',
+                'Programming Language :: Python',
+                'Topic :: Software Development',
+                'Topic :: Scientific/Engineering',
+                'Operating System :: Microsoft :: Windows',
+                'Operating System :: POSIX',
+                'Operating System :: Unix',
+                'Operating System :: MacOS',
+            ],
+            platforms='any',
             packages=PACKAGES,
-            package_data=PACKAGE_DATA,
             install_requires=REQUIRES,
-            python_requires=PYTHON_REQUIRES,
             setup_requires=SETUP_REQUIRES,
             requires=REQUIRES)
 
