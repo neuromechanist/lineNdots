9c5f01c9bc281d1ae47902070e0ad8b1628b0493
Seyed Yahya Shirazi <shirazi@ieee.org>
2024-03-23 08:54:20 -0700

    Implement unused inputs except flipped.

--- lineNdots/lineNdots.py
+++ lineNdots/lineNdots.py
@@ -7,9 +7,18 @@ import matplotlib.pyplot as plt
 def lnd(
     data, y, hue, x=None,
     agg_function=np.mean, var_function=np.std,
-    palette=None, ax=None, colors=None, line=None, dots=None, flipped=None, 
-    verbose=False, adtnl_space=0, intr_space=0, mean_size=0, size=0, lw=0, x_padding=0.1, legend=False
+    palette=None, ax=None, colors=None,
+    line=True, dots=True, dot_marker='o', flipped=False,
+    verbose=False, adtnl_space=0, mean_size=0, size=0, lw=0, x_padding=0.1, legend=False
 ) -> plt.Axes:
+    # Only one of the color and palette can be provided
+    if colors is not None and palette is not None:
+        raise ValueError('Only one of the color and palette can be provided')
+    if colors is not None:
+        palette = colors
+    if verbose is False:  # if verbose is False, suppress warnings
+        import warnings
+        warnings.filterwarnings("ignore")
     if ax is None:
         _, ax = plt.subplots(1, 1)
     if x is not None:
@@ -19,13 +28,14 @@ def lnd(
         ax.set_xticks(range(len(data[hue].unique())))
         ax.set_xticklabels(data[hue].unique())
 
+    # Do not advance iif line and dots are False
+    if not line and not dots:
+        return ax
+
     for i, art in enumerate(ax.collections):
         try:
             offsets = np.ma.getdata(art.get_offsets())
             agg = agg_function(offsets, axis=0)  # first number is x (agg position), second is y (agg value)
-            # The line `varx = var_function(offsets, axis=0)` is calculating the
-            # variability measure of the data points along the specified axis
-            # (axis=0 in this case).
             varx = var_function(offsets, axis=0)  # varx is the variability measure
             if x is not None:
                 if agg[0] < i // 2:
@@ -39,10 +49,12 @@ def lnd(
                 agg[0] = i + adtnl_space
 
             art.set_facecolor(palette[i % 2])
-            ax.plot([agg[0], agg[0]], [agg[1] - varx[1], agg[1] + varx[1]],
-                    color=art.get_facecolor()[0], lw=lw)
-            ax.plot(agg[0], agg[1], markersize=mean_size, marker='o',
-                    markeredgecolor=art.get_facecolor()[0], markerfacecolor='w', markeredgewidth=lw + 1)
+            if line:
+                ax.plot([agg[0], agg[0]], [agg[1] - varx[1], agg[1] + varx[1]],
+                        color=art.get_facecolor()[0], lw=lw)
+            if dots:
+                ax.plot(agg[0], agg[1], markersize=mean_size, marker=dot_marker,
+                        markeredgecolor=art.get_facecolor()[0], markerfacecolor='w', markeredgewidth=lw + 1)
         except Exception:
             if verbose:
                 print('empty collection encountered')
