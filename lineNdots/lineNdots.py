9c03533532922eb93915e441ac0d8b638992110c
Seyed Yahya Shirazi <shirazi@ieee.org>
2024-07-01 09:04:55 -0700

    Add hairlines (currently w/o mods)

--- lineNdots/lineNdots.py
+++ lineNdots/lineNdots.py
@@ -8,7 +8,8 @@ def lnd(
     agg_function=np.mean, var_function=np.std,
     palette=None, ax=None, colors=None,
     line=True, dots=True, dot_marker='o', flipped=False,
-    verbose=False, adtnl_space=0, mean_size=0, size=0, lw=0, x_padding=0.1, legend=False
+    verbose=False, adtnl_space=0, mean_size=0, size=0, lw=0, x_padding=0.1, legend=False,
+    hairlines=False  # New parameter to control hairlines
 ) -> plt.Axes:
     # Only one of the color and palette can be provided
     if colors is not None and palette is not None:
@@ -27,9 +28,12 @@ def lnd(
         ax.set_xticks(range(len(data[hue].unique())))
         ax.set_xticklabels(data[hue].unique())
 
-    # Do not advance iif line and dots are False
+    # Do not advance if line and dots are False
     if not line and not dots:
         return ax
+
+    hue_values = data[hue].unique()
+    scatter_offsets = {hue_val: [] for hue_val in hue_values}
 
     for i, art in enumerate(ax.collections):
         try:
@@ -49,6 +53,8 @@ def lnd(
                 art.set_offsets(offsets - [agg[0] - i + adtnl_space * mux, 0])
                 agg[0] = i + adtnl_space * mux
 
+            scatter_offsets[hue_values[i % len(hue_values)]].append(art.get_offsets())
+
             art.set_facecolor(palette[i % len(palette)])
             if line:
                 ax.plot([agg[0], agg[0]], [agg[1] - varx[1], agg[1] + varx[1]],
@@ -59,6 +65,14 @@ def lnd(
         except Exception as e:
             if verbose:
                 print(f'Error processing collection: {e}')
+
+    # Draw hairlines for paired comparisons
+    if hairlines and len(hue_values) == 2:
+        for category in data[x].unique():
+            red_offsets = np.vstack([offset for offset in scatter_offsets[hue_values[0]]])
+            blue_offsets = np.vstack([offset for offset in scatter_offsets[hue_values[1]]])
+            for red, blue in zip(red_offsets, blue_offsets):
+                ax.plot([red[0], blue[0]], [red[1], blue[1]], color='gray', linestyle='--', linewidth=0.5)
 
     # Set the x-axis range to include all the data and adjust for horizontal padding
     ax.set_xlim(ax.get_xlim()[0] - mux * 0.1, ax.get_xlim()[1] + 0.2 - mux * 0.1)
