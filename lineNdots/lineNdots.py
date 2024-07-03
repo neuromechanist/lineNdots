ce5a4d13cdd192f4d8bc54580d83fa2319ec6d5d
Seyed Yahya Shirazi <shirazi@ieee.org>
2024-07-03 14:53:15 -0700

    Add hairline options

--- lineNdots/lineNdots.py
+++ lineNdots/lineNdots.py
@@ -8,8 +8,8 @@ def lnd(
     agg_function=np.mean, var_function=np.std,
     palette=None, ax=None, colors=None,
     line=True, dots=True, dot_marker='o', flipped=False,
-    verbose=False, adtnl_space=0, mean_size=0, size=0, lw=0, x_padding=0.1, legend=False,
-    hairlines=False  # New parameter to control hairlines
+    verbose=False, adtnl_space=0.1, mean_size=20, size=10, lw=2, x_padding=0.1, legend=False,
+    hairlines=False, hairline_color='gray', hairline_style='--', hairline_width=0.5
 ) -> plt.Axes:
     # Only one of the color and palette can be provided
     if colors is not None and palette is not None:
@@ -72,7 +72,8 @@ def lnd(
             red_offsets = np.vstack([offset for offset in scatter_offsets[hue_values[0]]])
             blue_offsets = np.vstack([offset for offset in scatter_offsets[hue_values[1]]])
             for red, blue in zip(red_offsets, blue_offsets):
-                ax.plot([red[0], blue[0]], [red[1], blue[1]], color='gray', linestyle='--', linewidth=0.5)
+                ax.plot([red[0], blue[0]], [red[1], blue[1]],
+                        color=hairline_color, linestyle=hairline_style, lw=hairline_width)
 
     # Set the x-axis range to include all the data and adjust for horizontal padding
     ax.set_xlim(ax.get_xlim()[0] - mux * 0.1, ax.get_xlim()[1] + 0.2 - mux * 0.1)
